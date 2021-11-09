cecfda2837d96651c323f3298fcc12b7ac931fa6
Loic Rouaud <loic.rouaud@gmail.com>
2021-11-10 00:06:50 +0100

    Python_Functions_Project-insurance#2

--- 2-Python_Functions_Medical_Project.py
+++ 2-Python_Functions_Medical_Project.py
@@ -1,22 +1,15 @@
 # Create calculate_insurance_cost() function below: 
-def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
-  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print("The estimated insurance cost for " + name + " is " + str(estimated_cost) + " dollars.")
+def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker,):
+  estimaded_cost = (250*age)+(128*sex)+(370*bmi)+(425*num_of_children)+(24000*smoker)-12500
+  return estimaded_cost, print("The estimaded insurance cost for "+ name +" is "+str(estimaded_cost)+" dollars")
 
-# Estimate Maria's insurance cost
-maria_insurance_cost = calculate_insurance_cost(name = "Maria", age = 28, sex = 0, bmi = 26.2, num_of_children = 3, smoker = 0)
+def dif_insurance_cost(cost1, cost2):
+  dif = cost1 - cost2
+  return dif, print("The difference in insurance cost is "+ str(dif) +" dollars.")
 
-# Estimate Omar's insurance cost 
-omar_insurance_cost = calculate_insurance_cost(name = "Omar", age = 35, sex = 1, bmi = 22.2, num_of_children = 0, smoker = 1)
+maria_insurance_cost =calculate_insurance_cost("Maria", 28, 0, 26.2, 3, 0)
+omar_insurance_cost = calculate_insurance_cost("Omar", 35, 1, 22.2, 0, 1)
 
-# Estimate of our insurance cost 
-loic_insurance_cost = calculate_insurance_cost(name = "Loic", age = 34, sex = 0, bmi = 26.2, num_of_children = 0, smoker = 0)
+my_insurance_cost = calculate_insurance_cost("Loic", 34, 1, 26, 0, 0)
 
-print(omar_insurance_cost)
-
-# Extra_difference in costs
-def difference_cost(cost1, cost2):
-  diff = cost1 - cost2
-  print(f"The difference in insurance cost is {cost1-cost2} dollars.")
-
-difference = difference_cost(omar_insurance_cost,maria_insurance_cost)
+omar_maria_diference = dif_insurance_cost(omar_insurance_cost[0], maria_insurance_cost[0])
